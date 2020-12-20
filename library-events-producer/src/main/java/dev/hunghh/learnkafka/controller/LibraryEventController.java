d588e1268ea0a76a92120c71eca4d48e8443677e
Hunghh.dev <hunghh.dev@gmail.com>
2020-12-20 17:56:03 +0700

    Producer using KafkaTemplate

--- library-events-producer/src/main/java/dev/hunghh/learnkafka/controller/LibraryEventController.java
+++ library-events-producer/src/main/java/dev/hunghh/learnkafka/controller/LibraryEventController.java
@@ -1,18 +1,26 @@
 package dev.hunghh.learnkafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import dev.hunghh.learnkafka.domain.LibraryEvent;
+import dev.hunghh.learnkafka.producer.LibraryEventProducer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class LibraryEventController {
 
+    @Resource
+    LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/library-event")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         // invoke kafka producer
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
---
+++ library-events-producer/src/main/java/dev/hunghh/learnkafka/producer/LibraryEventProducer.java
@@ -0,0 +1,54 @@
+package dev.hunghh.learnkafka.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.hunghh.learnkafka.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import javax.annotation.Resource;
+
+@Component
+@Slf4j
+public class LibraryEventProducer {
+
+    @Resource
+    KafkaTemplate<Integer, String> kafkaTemplate;
+    @Resource
+    ObjectMapper objectMapper;
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        ListenableFuture<SendResult<Integer, String>> libraryFuture = kafkaTemplate.sendDefault(key, value);
+        libraryFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, value, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+        });
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error Sending the Message and the exception is {} ", ex.getMessage());
+        try {
+            throw ex;
+        } catch (Throwable throwable) {
+            log.error("Error in OnFailure: {}", throwable.getMessage());
+        }
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message Sent SuccessFully for the key : {} and the value is {} , partition is {}", key, value, result.getProducerRecord().partition());
+    }
+}
