88500a60feb715aad10b5381fd9bf7dc7517d48a
Hunghh.dev <hunghh.dev@gmail.com>
2020-12-22 09:58:00 +0700

    Set Up the Library Events Consumer Base project

---
+++ library-events-consumer/src/test/java/dev/hunghh/learnkafka/LibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hunghh.learnkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
