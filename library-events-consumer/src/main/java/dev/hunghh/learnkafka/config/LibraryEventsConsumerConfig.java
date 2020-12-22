e71f17229500206ae266c6e391fdac7af3d15b50
Hunghh.dev <hunghh.dev@gmail.com>
2020-12-22 10:00:13 +0700

    Build Kafka Consumer using @KafkaListener Annotation

---
+++ library-events-consumer/src/main/java/dev/hunghh/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,10 @@
+package dev.hunghh.learnkafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+
+}
