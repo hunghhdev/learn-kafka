e808af7ad930f82f2d707f0c23de7ca427fd9b46
Hunghh.dev <hunghh.dev@gmail.com>
2020-12-23 09:00:32 +0700

    Manual Consumer Offset Management

--- library-events-consumer/src/main/java/dev/hunghh/learnkafka/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/dev/hunghh/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,10 +1,25 @@
 package dev.hunghh.learnkafka.config;
 
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
 
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory
+    ){
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
 }
