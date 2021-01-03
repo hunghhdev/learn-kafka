cfbc27990a37d820a449cc26868e12570e4b869c
Hunghh.dev <hunghh.dev@gmail.com>
2021-01-03 19:46:46 +0700

    retry

--- library-events-consumer/src/main/java/dev/hunghh/learnkafka/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/dev/hunghh/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @EnableKafka
@@ -25,6 +29,23 @@ public class LibraryEventsConsumerConfig {
         factory.setErrorHandler((throwException,data)->{
             log.info("Exception in consumerConfig is {} amd the record is {}", throwException.getMessage(), data);
         });
+        factory.setRetryTemplate(retryTemplate());
         return factory;
+    }
+
+    private RetryTemplate retryTemplate() {
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        return retryTemplate;
+    }
+
+    private RetryPolicy simpleRetryPolicy() {
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+        return simpleRetryPolicy;
     }
 }
